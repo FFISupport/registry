3710aad174772f07548c57b71a53396c13c8887e
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-05-23 14:11:24 +0800

    chore: eslint, prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./globals.css";
-import { FC, ReactNode } from "react";
+import type { FC, ReactNode } from "react";
 import { lexend } from "@/utils/styles/font";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
