6fc9e995a39404e36165fbca075bfd29ff5e2b00
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-06-03 15:29:12 +0800

    feat: initial upload modal

---
+++ src/components/Upload/UploadModal.tsx
@@ -0,0 +1,7 @@
+export const UploadModal = () => {
+    return (
+        <div className="fixed inset-0 z-50 flex min-h-screen items-center justify-center bg-black/30 backdrop-blur-sm">
+            Hi!
+        </div>
+    );
+};
--- src/components/Upload/UploadNavButton.tsx
+++ src/components/Upload/UploadNavButton.tsx
@@ -3,18 +3,18 @@
 import { LucidePlus } from "@/components/Icons/LucidePlus";
 import { AnimatedButton } from "@/components/Animated/AnimatedButton";
 import { useState } from "react";
+import { UploadModal } from "@/components/Upload/UploadModal";
 
 export const UploadNavButton = () => {
+    const [showModal, setShowModal] = useState(false);
+
     const handleClick = () => {
         setShowModal(true);
     };
 
     return (
-        <AnimatedButton className="bg-ctp-l-green dark:bg-ctp-green flex cursor-pointer items-center justify-center gap-1 rounded-lg p-2">
-            <LucidePlus className="dark:text-ctp-crust text-ctp-l-crust" />
-            <p className="dark:text-ctp-crust text-ctp-l-crust text-sm">New</p>
-        </AnimatedButton>
         <div className="">
+            {showModal && <UploadModal />}
             <AnimatedButton
                 className="bg-ctp-l-green dark:bg-ctp-green flex cursor-pointer items-center justify-center gap-1 rounded-lg p-2"
                 onClick={handleClick}
