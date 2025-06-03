43f37f4a329badd78b3670882c2b1d01f61ec95d
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-06-03 15:28:56 +0800

    refactor: provide onclick handler

--- src/components/Animated/AnimatedButton.tsx
+++ src/components/Animated/AnimatedButton.tsx
@@ -1,23 +1,30 @@
 import { motion } from "motion/react";
-import type { ReactNode } from "react";
+import type { MouseEventHandler, ReactNode } from "react";
 
 export interface AnimButtonProps {
     children: ReactNode;
     className?: string;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 /**
  * Reusable animated button component. Use this in place of a normal <button> div whenever you need to apply default animations to it.
  * @param children - Provide it within markup. DO NOT pass this as an inline prop. This component should wrap other components to create the button.
  * @param className - CSS class name(s) in string
+ * @param onClick - On click handler
  */
 
-export const AnimatedButton = ({ children, className }: AnimButtonProps) => {
+export const AnimatedButton = ({
+    children,
+    className,
+    onClick,
+}: AnimButtonProps) => {
     return (
         <motion.button
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             className={className}
+            onClick={onClick}
         >
             {children}
         </motion.button>
--- src/components/Upload/UploadNavButton.tsx
+++ src/components/Upload/UploadNavButton.tsx
@@ -2,12 +2,28 @@
 
 import { LucidePlus } from "@/components/Icons/LucidePlus";
 import { AnimatedButton } from "@/components/Animated/AnimatedButton";
+import { useState } from "react";
 
 export const UploadNavButton = () => {
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
     return (
         <AnimatedButton className="bg-ctp-l-green dark:bg-ctp-green flex cursor-pointer items-center justify-center gap-1 rounded-lg p-2">
             <LucidePlus className="dark:text-ctp-crust text-ctp-l-crust" />
             <p className="dark:text-ctp-crust text-ctp-l-crust text-sm">New</p>
         </AnimatedButton>
+        <div className="">
+            <AnimatedButton
+                className="bg-ctp-l-green dark:bg-ctp-green flex cursor-pointer items-center justify-center gap-1 rounded-lg p-2"
+                onClick={handleClick}
+            >
+                <LucidePlus className="dark:text-ctp-crust text-ctp-l-crust" />
+                <p className="dark:text-ctp-crust text-ctp-l-crust text-sm">
+                    New
+                </p>
+            </AnimatedButton>
+        </div>
     );
 };
