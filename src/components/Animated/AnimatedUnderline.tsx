c92a8701deee25c47fa736a1ec63b7bb4918e507
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-06-03 14:59:10 +0800

    refactor(css)!: provide variants in enum

--- src/components/Animated/AnimatedUnderline.tsx
+++ src/components/Animated/AnimatedUnderline.tsx
@@ -2,28 +2,11 @@
 
 import { motion } from "motion/react";
 import type { ReactNode } from "react";
+import { CatppuccinBackgroundColour } from "@/utils/styles/catppuccin";
 
 interface AnimatedUnderlineProps {
     children: ReactNode;
-    colour?: AnimatedUnderlineColour;
-}
-
-export enum AnimatedUnderlineColour {
-    ROSEWATER = "dark:bg-ctp-rosewater bg-ctp-l-rosewater",
-    FLAMINGO = "dark:bg-ctp-flamingo bg-ctp-l-flamingo",
-    PINK = "dark:bg-ctp-pink bg-ctp-l-pink",
-    MAUVE = "dark:bg-ctp-mauve bg-ctp-l-mauve",
-    RED = "dark:bg-ctp-red bg-ctp-l-red",
-    MAROON = "dark:bg-ctp-maroon bg-ctp-l-maroon",
-    PEACH = "dark:bg-ctp-peach bg-ctp-l-peach",
-    YELLOW = "dark:bg-ctp-yellow bg-ctp-l-yellow",
-    GREEN = "dark:bg-ctp-green bg-ctp-l-green",
-    TEAL = "dark:bg-ctp-teal bg-ctp-l-teal",
-    SKY = "dark:bg-ctp-sky bg-ctp-l-sky",
-    SAPPHIRE = "dark:bg-ctp-sapphire bg-ctp-l-sapphire",
-    BLUE = "dark:bg-ctp-blue bg-ctp-l-blue",
-    LAVENDER = "dark:bg-ctp-lavender bg-ctp-l-lavender",
-    TEXT = "dark:bg-ctp-text bg-ctp-l-text",
+    colour?: CatppuccinBackgroundColour;
 }
 
 /**
@@ -34,7 +17,7 @@ export enum AnimatedUnderlineColour {
  */
 export const AnimatedUnderline = ({
     children,
-    colour = AnimatedUnderlineColour.TEXT,
+    colour = CatppuccinBackgroundColour.TEXT,
 }: AnimatedUnderlineProps) => {
     return (
         <motion.div
--- src/components/Buttons/BackButton.tsx
+++ src/components/Buttons/BackButton.tsx
@@ -2,10 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { LucideCornerDownLeft } from "@/components/Icons/LucideCornerDownLeft";
-import {
-    AnimatedUnderline,
-    AnimatedUnderlineColour,
-} from "@/components/Animated/AnimatedUnderline";
+import { AnimatedUnderline } from "@/components/Animated/AnimatedUnderline";
+import { CatppuccinBackgroundColour } from "@/utils/styles/catppuccin";
 
 export const BackButton = () => {
     const router = useRouter();
@@ -20,7 +18,7 @@ export const BackButton = () => {
             onClick={handleClick}
             className="cursor-pointer p-4 pl-6"
         >
-            <AnimatedUnderline colour={AnimatedUnderlineColour.SKY}>
+            <AnimatedUnderline colour={CatppuccinBackgroundColour.SKY}>
                 <div className="flex items-center gap-2">
                     <LucideCornerDownLeft className="dark:text-ctp-sky text-ctp-l-sky" />
                     <p>Back</p>
