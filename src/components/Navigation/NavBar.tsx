9b7f8044c1b5c86d21cb1d8c20f7a11eab194d52
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-06-03 14:00:17 +0800

    feat: initial navbar

---
+++ src/components/Navigation/NavBar.tsx
@@ -0,0 +1,16 @@
+import { FFILogo } from "@/components/Icons/FFILogo";
+
+export const NavBar = () => {
+    return (
+        <div className="dark:border-b-ctp-surface-1 border-b-ctp-l-surface-1 flex min-w-dvh items-center justify-between gap-2 border-b p-4 pr-8 pl-8">
+            <a href="/home">
+                <div className="flex items-center justify-center gap-2 align-middle">
+                    <FFILogo className="dark:text-ctp-sky text-ctp-l-sky" />
+                    <h1 className="text-lg font-semibold">Registry</h1>
+                </div>
+            </a>
+
+            <p>Hi</p>
+        </div>
+    );
+};
